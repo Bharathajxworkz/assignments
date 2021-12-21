8b3b4d4a356698a9d64fb6488e2975b869bfe630
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-21 23:09:43 +0530

    Fornumseries is added

---
+++ Fornumseries.java
@@ -0,0 +1,22 @@
+class Fornumseries{
+	public static void main(String[] args){
+
+	int s=0;
+	for(int number = 0; number<=10; number++){
+	s=s+number;
+	System.out.println(s);
+
+
+
+
+
+
+
+
+
+
+}
+
+}
+
+}
