e8e84b746bf2ed0a38cbbbcbf15b2b9e33174686
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-22 20:25:46 +0530

    using for loop

---
+++ Weekdays.java
@@ -0,0 +1,11 @@
+class Weekdays{
+	public static void main(String[] args){
+		String[] days ={"Sunday","Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"};
+
+		for(int index = 0; index < days.length ;index++ ){
+		System.out.println(days[index]);
+
+
+}
+}
+}
