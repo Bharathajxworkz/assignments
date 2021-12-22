d83f800363a9842e1baa1f4c8b9aef5a3d5d8993
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-22 20:29:43 +0530

    execute by using ++

---
+++ Year.java
@@ -0,0 +1,15 @@
+class Year{
+
+	public static void main(String[] args){
+
+		String[] month= {"January","February","March","April","May","June","July","August","September","October","November","Decmber"};
+
+
+
+		System.out.println(month[0]+" "+month[1]+" "+month[2]+" "+month[3]+" "+month[4]+" "+month[5]+" "+month[6]+" "+month[7]+" "+month[8]+" "+month[9]+" "+month[10]+" "+month[11]);
+
+
+
+
+}
+}
