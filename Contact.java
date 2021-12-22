88bc4acaa0410f08db363d501bdfa0701de849b0
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-22 20:11:38 +0530

    by using for loop

---
+++ Contact.java
@@ -0,0 +1,16 @@
+class Contact{
+	public static void main(String[] args){
+
+		long[] phonenum = {7816960424l,6363977693l,7338065029l,9110849906l,7026329503l,7026637798l,9743675131l};
+		
+		for(int index = 0;index < phonenum.length;index++){
+
+			System.out.println(phonenum[index]);
+
+
+
+
+
+}
+}
+}
