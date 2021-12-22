1347dcff235160c914d1c61b3467ea3250a4411b
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-22 20:18:10 +0530

    exeuite the sop with ternary

---
+++ Contacts.java
@@ -0,0 +1,14 @@
+class Contacts{
+	public static void main(String[] args){
+
+		String[] phonenum = {"7816960424","6363977693","7338065029","9110849906","7026329503","7026637798","9743675131"};
+		
+
+			System.out.println(phonenum[0]+" "+phonenum[1]+" "+phonenum[2]+" "+phonenum[3]+" "+phonenum[4]+" "+phonenum[5]+" "+phonenum[6]);
+
+
+
+
+
+}
+}
