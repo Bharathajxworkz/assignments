dc227692cfb03b72d09552c2a610aa759ee6519d
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-22 20:27:31 +0530

    using for loop

---
+++ Month.java
@@ -0,0 +1,17 @@
+class Month{
+
+	public static void main(String[] args){
+
+		String[] masa = {"January","February","March","April","May","June","July","August","September","October","November","Decmber"};
+
+		for(int index = 0;index < masa.length;index =index+1){
+
+
+
+		System.out.println(masa[index]);
+
+
+}
+
+}
+}
