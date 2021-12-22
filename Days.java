735e89da231783d29c2cf13921005485f5556e3b
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-22 20:20:26 +0530

    execute the sop with ternary

---
+++ Days.java
@@ -0,0 +1,10 @@
+class Days{
+	public static void main(String[] args){
+		String[] weekdays ={"Sunday","Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"};
+
+		System.out.println(weekdays[0]+""+weekdays[1]+""+weekdays[2]+""+weekdays[3]+""+weekdays[4]+""+weekdays[5]+""+weekdays[6]);
+
+
+
+}
+}
